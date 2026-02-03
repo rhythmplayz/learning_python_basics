0ad6f7ad74f265379d0212d270e538e4061d8502
rhythmplayz <114134549+rhythmplayz@users.noreply.github.com>
2026-02-04 05:46:24 +0600

    basic python data types

---
+++ 04_data_types/01_num_n_bool.py
@@ -0,0 +1,12 @@
+num_int = 10
+num_float = 3.14
+num_complex = 1 + 5j
+t_bool = True
+print(num_int)
+print(type(num_int))
+print(num_float)
+print(type(num_float))
+print(num_complex)
+print(type(num_complex))
+print(t_bool)
+print(type(t_bool))
---
+++ 04_data_types/02_sequencial.py
@@ -0,0 +1,24 @@
+#string
+s = "Hello World"
+print(s)
+print(type(s))
+
+#list
+a = [1, 2, 3]
+print(a)
+print(type(a))
+
+#tuple
+a = (1, 2, 3)
+print(a)
+print(type(a))
+
+#set
+a = {1,1,2,3,4,4,5}
+print(a)
+print(type(a))
+
+#dictionary
+a = {1: 'a', 2: 'b', 3: 'c'}
+print(a)
+print(type(a))
