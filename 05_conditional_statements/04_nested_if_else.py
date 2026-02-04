37ea5c39d67c95fee9fb65ed5f3d4af802798520
rhythmplayz <114134549+rhythmplayz@users.noreply.github.com>
2026-02-04 06:05:00 +0600

    basic python nest if..else ternary

---
+++ 05_conditional_statements/04_nested_if_else.py
@@ -0,0 +1,43 @@
+age = 70
+is_member = True
+
+if age >= 60:
+    if is_member:
+        print("30% senior discount!")
+    else:
+        print("20% senior discount.")
+else:
+    print("Not eligible for a senior discount.")
+
+age = 70
+is_member = False
+
+if age >= 60:
+    if is_member:
+        print("30% senior discount!")
+    else:
+        print("20% senior discount.")
+else:
+    print("Not eligible for a senior discount.")
+
+age = 30
+is_member = True
+
+if age >= 60:
+    if is_member:
+        print("30% senior discount!")
+    else:
+        print("20% senior discount.")
+else:
+    print("Not eligible for a senior discount.")
+
+age = 30
+is_member = False
+
+if age >= 60:
+    if is_member:
+        print("30% senior discount!")
+    else:
+        print("20% senior discount.")
+else:
+    print("Not eligible for a senior discount.")
---
+++ 05_conditional_statements/05_ternary.py
@@ -0,0 +1,25 @@
+age = 20
+s = "Adult" if age >= 18 else "Minor"
+print(s)
+
+age = 10
+s = "Adult" if age >= 18 else "Minor"
+print(s)
+
+a = 10
+b = 20
+c = 30
+s = "c bigger" if c>a and c>b else "b bigger" if b>a else "a bigger"
+print(s)
+
+a = 10
+b = 30
+c = 20
+s = "c bigger" if c>a and c>b else "b bigger" if b>a else "a bigger"
+print(s)
+
+a = 30
+b = 20
+c = 10
+s = "c bigger" if c>a and c>b else "b bigger" if b>a else "a bigger"
+print(s)
