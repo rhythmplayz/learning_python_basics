b133cf151e26eed38bbda20006f3be3392f244f7
rhythmplayz <114134549+rhythmplayz@users.noreply.github.com>
2026-02-27 01:28:22 +0600

    basic python loop control statements

---
+++ 06_loops/04_loop_control_statement.py
@@ -0,0 +1,17 @@
+for ch in "Rhythm":
+    if ch == "h":
+        continue
+    print(ch)
+
+print()
+
+for ch in "Rhythm":
+    if ch == "y":
+        break
+    print(ch)
+
+print()
+
+for ch in "Rhythm":
+    pass
+print(ch)
