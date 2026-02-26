7358f542d1e641de34118ffc635f94be1f456c57
rhythmplayz <114134549+rhythmplayz@users.noreply.github.com>
2026-02-27 01:18:43 +0600

    basic python for and while loop

---
+++ 06_loops/01_for_loop.py
@@ -0,0 +1,42 @@
+n = 10
+
+for i  in range(n):
+    print(i)
+
+print()
+for i  in range(2,n):
+    print(i)
+
+print()
+for i in range(2,n,2):
+    print(i)
+
+print()
+li = [1,2.2,"HI"]
+for item in li:
+    print(item)
+
+print()
+tup = (1,2.2,"HI")
+for item in tup:
+    print(item)
+
+print()
+s = "I'm Rhythm"
+for char in s:
+    print(char)
+
+print()
+dicti = {1:123, 2:"HI", "HI":"Hello World!"}
+for key in dicti:
+    print(dicti[key])
+
+print()
+st = {10,10,30,20,40}
+for item in st:
+    print(item)
+
+print()
+li = [1,2.2,"HI"]
+for i in range(len(li)):
+    print(li[i])
---
+++ 06_loops/02_while_loop.py
@@ -0,0 +1,13 @@
+cnt = 10
+
+while cnt > 0:
+    print(cnt)
+    cnt-=1
+
+"""Infinite loop
+
+while True:
+    print("HI")
+
+"""
+
